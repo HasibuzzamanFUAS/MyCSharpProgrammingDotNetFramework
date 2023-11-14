fa5ebd0d07dc0b414bd6aa031557633f80d06e70
HASIBUZZAMAN <hasibniloy14@gmail.com>
2023-11-14 22:25:43 +0100

    This is my Write Print Read Code, Thanks!

---
+++ WritePrintRead/WritePrintRead/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace WritePrintRead
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is my Write Print Read Code");
+            Console.ReadLine();
+        }
+    }
+}
