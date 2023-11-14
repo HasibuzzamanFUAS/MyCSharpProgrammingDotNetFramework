94c8a80c47114a3dac24e357b2a55ea327f2e294
HASIBUZZAMAN <hasibniloy14@gmail.com>
2023-11-14 23:15:57 +0100

    Taking Input from User Basic code

---
+++ TakeInputFromUser/TakeInputFromUser/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeInputFromUser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name: ");
+            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine("Please Enter Your Age: ");
+            Console.WriteLine(Console.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}
